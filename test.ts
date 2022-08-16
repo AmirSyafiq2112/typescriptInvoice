008ab00311dd116f1dab013bbb6b0ef2e5e63473
AmirSyafiq2112 <amirabrashid@yahoo.com>
2022-08-16 17:05:57 +0800

    docs:add ENUM notes in test.ts

--- test.ts
+++ test.ts
@@ -76,3 +76,32 @@
 // greetPerson(me);
 
 // console.log(me);
+
+// ENUMS
+
+// enum ResourceType {
+// 	BOOK,
+// 	AUTHOR,
+// 	FILM,
+// 	DIRECTOR,
+// 	PERSON,
+// }
+
+// interface Resource<T> {
+// 	uid: number;
+// 	resourceType: ResourceType;
+// 	data: T;
+// }
+
+// const docOne: Resource<object> = {
+// 	uid: 1,
+// 	resourceType: ResourceType.BOOK,
+// 	data: { title: "name of the wind" },
+// };
+// const docTwo: Resource<object> = {
+// 	uid: 10,
+// 	resourceType: ResourceType.PERSON,
+// 	data: { name: "yoshi" },
+// };
+
+// console.log(docOne, docTwo);
